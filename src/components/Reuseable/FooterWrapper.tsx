c2e751b4a59f29a31244dd52c8adfd716f071750
developerMasum <developer.masum2001@gmail.com>
2025-08-16 23:14:19 +0600

    ff

--- src/components/Reuseable/FooterWrapper.tsx
+++ src/components/Reuseable/FooterWrapper.tsx
@@ -1,10 +1,10 @@
-"use client";
+// "use client";
 
-import { usePathname } from "next/navigation";
-import Footer from "./Footer";
+// import { usePathname } from "next/navigation";
+// import Footer from "./Footer";
 
-export default function FooterWrapper() {
-  const pathname = usePathname();
-  if (pathname === "/") return null;
-  return <Footer />;
-}
+// export default function FooterWrapper() {
+//   const pathname = usePathname();
+//   if (pathname === "/") return null;
+//   return <Footer />;
+// }
